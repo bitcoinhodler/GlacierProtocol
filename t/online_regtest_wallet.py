6e37a3872e933ee36db3fa52714936fb5b7cc473
Bitcoin Hodler <bitcoinhodler@safe-mail.net>
2019-12-13 04:24:16 +0000

    Show filename that caused ParseError

--- t/online_regtest_wallet.py
+++ t/online_regtest_wallet.py
@@ -655,8 +655,7 @@ class ParsedRunfile(metaclass=ABCMeta):
                 return klass(prefix, filename)
         raise RuntimeError("Unrecognized test input filename: " + filename)
 
-    @staticmethod
-    def parser_coroutine(contents):
+    def parser_coroutine(self, contents):
         """
         Accept a sequence of regexps that contents must match, yielding matched strings.
 
@@ -669,7 +668,7 @@ class ParsedRunfile(metaclass=ABCMeta):
             regexp = yield match.group() if match else None
             match = re.match(regexp, contents, re.DOTALL | re.MULTILINE | re.VERBOSE)
             if not match:
-                raise ParseError("did not match expected regexp")
+                raise ParseError("did not match expected regexp in " + self.filename)
             contents = contents[match.end():]
         yield match.group()  # Last group...shouldn't ever return
 
